bf2715d339a8830d97595389a6a124f5bfcffa01
CoolHappy <narinder99143@gmail.com>
2022-11-15 17:10:44 +0530

    updated index file

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ 
+</head>
+<body>
 <?php
 
 echo"hello world";
 echo"it is second commit"; 
 echo" stash testing fine or not ";
+?>
+
+</body>
+</html>
 
